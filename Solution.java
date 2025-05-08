6678d885a64e1ab07c6793f4d7f65589bf7202d3
Bhargav11234 <21it137@charusat.edu.in>
2025-05-08 11:35:48 +0530

    DFS algorithm to detect a cycle

---
+++ Solution.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+class Solution {
+    private boolean dfsCheck(int node, List<List<Integer>> adj, int[] vis, int[] pathVis) {
+        vis[node] = 1;
+        pathVis[node] = 1;
+
+        List<Integer> neighbors = adj.get(node);
+        for (int i = 0; i < neighbors.size(); i++) {
+            int it = neighbors.get(i);
+
+            if (vis[it] == 0) {
+                if (dfsCheck(it, adj, vis, pathVis)) {
+                    return true;
+                }
+            } else if (pathVis[it] == 1) {
+                return true;
+            }
+        }
+
+        pathVis[node] = 0;
+        return false;
+    }
+
+    public boolean isCyclic(int V, List<List<Integer>> adj) {
+        int[] vis = new int[V];
+        int[] pathVis = new int[V];
+
+        for (int i = 0; i < V; i++) {
+            if (vis[i] == 0) {
+                if (dfsCheck(i, adj, vis, pathVis)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int V = 4; // total node
+        List<List<Integer>> adj = new ArrayList<>();
+
+        // Initialize adjacency list
+        for (int i = 0; i < V; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        // 0 -> 1, 1 -> 2, 2 -> 3, 3 -> 1 
+        adj.get(0).add(1);
+        adj.get(1).add(2);
+        adj.get(2).add(3);
+        adj.get(3).add(1);
+
+        Solution obj = new Solution();
+        if (obj.isCyclic(V, adj)) {
+            System.out.println("Yes detect a Cycle");
+        } else {
+            System.out.println("No Cycle");
+        }
+    }
+}
